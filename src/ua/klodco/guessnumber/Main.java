7d79a4f11bb54735b2b3b4716c114ca48f14eec6
Kostiantyn Kravchenko <stoutua@gmail.com>
2020-02-15 12:34:50 +0200

    simplified Main class

--- src/ua/klodco/guessnumber/Main.java
+++ src/ua/klodco/guessnumber/Main.java
@@ -3,8 +3,6 @@ package ua.klodco.guessnumber;
 public class Main {
 
     public static void main(String[] args) {
-//        Model model = new Model();
-//        View view = new View();
         Controller controller = new Controller(new Model(), new View());
         controller.gameIsOn();
     }
